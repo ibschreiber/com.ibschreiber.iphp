a5d0ff1cd66c508c17675c2dbbb8dc5a8075829a
Alex Frenkel <sirshurf@yahoo.com>
2011-02-28 18:50:17 +0800

    Added support for Zend_Json_Expr, 2 new abstract methods

--- Ingot/JQuery/JqGrid/Plugin/ToolbarFilter.php
+++ Ingot/JQuery/JqGrid/Plugin/ToolbarFilter.php
@@ -17,11 +17,9 @@ class Ingot_JQuery_JqGrid_Plugin_ToolbarFilter extends Ingot_JQuery_JqGrid_Plugi
 {
     protected $_options;
 	
-	protected static $_arrEvents = array("beforeSearch",	"afterSearch",	"beforeClear",	"afterClear");
-
     public function __construct($options = array())
     {
-        $this->_options = $options;
+		$this->setOptions($options);
     }
 
     public function preRender()
@@ -56,38 +54,13 @@ class Ingot_JQuery_JqGrid_Plugin_ToolbarFilter extends Ingot_JQuery_JqGrid_Plugi
     public function postResponse()
     {    // Not implemented
     }
-	
-	public function encodeJsonOptions($arrProperties) {
 		
-		$arrUnEscapeList = array_merge ( Ingot_JQuery_JqGrid_Plugin_ToolbarFilter::$_arrEvents );
-		
-		$strOptions = '';
-		
-		// Iterate over array
-		foreach ( ( array ) $arrProperties as $strPropertyKey => $mixProperty ) {
-			
-			if (! empty ( $strOptions )) {
-				$strOptions .= ", ";
-			}
-			// Check that it's not one of the elements that needs escaiping 	
-			if (in_array ( $strPropertyKey, $arrUnEscapeList, true )) {
-				// This value does not need escaiping
-				$strOptions .= '"' . $strPropertyKey . '":' . $mixProperty;
-			} else {
-				if (is_array ( $mixProperty )) {
-					// Recursive call
-					$strOptions .= '"' . $strPropertyKey . '":' . $this->encodeJsonOptions ( $mixProperty );
-				} else {
-					// @TODO add option to switch what JSON to use.
-					$strOptions .= '"' . $strPropertyKey . '":' . ZendX_JQuery::encodeJson ( $mixProperty );
-					//$strOptions .= '"' . $strPropertyKey . '":' . custom_json::encode ( $mixProperty );
-				}
-			
-			}
-		}
-		
-		$strOptions = "{" . $strOptions . "}";
-		
-		return $strOptions;
+	public function getMethods(){
+		return array();
 	}
+	
+	public function getEvents(){
+		return array("beforeSearch", "afterSearch",	"beforeClear",	"afterClear");
+	}
+	
 }
